793a523a0902f1d8d11ab2750bd872edad512107
overlord <dym8750@gmail.com>
2019-07-19 17:22:50 +0300

    Новый проэкт

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById("trigger").onclick = function() {
+    open()
+  };
+  
+  function open() {
+    document.getElementById("menu").classList.toggle("show");
+    document.getElementById("trigger").classList.toggle("active");
+  }
+
+
